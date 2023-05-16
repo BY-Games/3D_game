6aa5569e74b3a8ad994be9cbe5c316010183b982
yosefkahlon <yosefkahlon53@gmail.com>
2023-05-16 15:59:30 +0300

    new scenes

--- Assets/Scripts/1-player/Shooter.cs
+++ Assets/Scripts/1-player/Shooter.cs
@@ -7,70 +7,86 @@ using UnityEngine;
  * This component represents a player that can shoot using left-click and reload using R.
  * The player has limited ammunition.
  */
-public class Shooter : MonoBehaviour {
+public class Shooter : MonoBehaviour
+{
     private Actions actions;
     private AimStateManager aimState;
 
-    [Tooltip("Particle-effect that is triggered near the weapon mouth when the player shoots")] [SerializeField]
-    private ParticleSystem muzzleFlash = null;
+    [Tooltip("Particle-effect that is triggered near the weapon mouth when the player shoots")]
+    [SerializeField]
+    private GameObject muzzleFlash = null;
 
-    [Tooltip("Particle-effect that is triggered where the player's bullet hits")] [SerializeField]
+    [Tooltip("Particle-effect that is triggered where the player's bullet hits")]
+    [SerializeField]
     private GameObject bulletHole = null;
 
-    [Tooltip("How many bullets the player initially has")] [SerializeField]
+    [Tooltip("How many bullets the player initially has")]
+    [SerializeField]
     private int startAmmo = 50;
 
-    [Tooltip("How many bullets the player currently has")] [SerializeField]
+    [Tooltip("How many bullets the player currently has")]
+    [SerializeField]
     private int ammo;
 
 
     private bool _isReloading;
 
-    void Start() {
+    void Start()
+    {
         ammo = startAmmo;
-        if (muzzleFlash) {
-            // muzzleFlash.SetActive(false);
+        if (muzzleFlash)
+        {
+            muzzleFlash.SetActive(false);
         }
-        
+
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         actions = GetComponent<Actions>();
         aimState = GetComponent<AimStateManager>();
     }
 
-    void Update() {
-        if (Input.GetMouseButton(0) && !_isReloading) {
-            if (ammo > 0) {
-                if (actions) {
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !_isReloading)
+        {
+            if (ammo > 0)
+            {
+                if (actions)
+                {
                     actions.Attack();
                 }
 
                 // actions.SendMessage("Attack", SendMessageOptions.DontRequireReceiver);
                 Shoot();
             }
-            else {
+            else
+            {
                 Debug.Log("Out of ammunition!");
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.R) && !_isReloading) {
+        if (Input.GetKeyDown(KeyCode.R) && !_isReloading)
+        {
             _isReloading = true;
             Reload();
         }
     }
 
-    private void Shoot() {
-        muzzleFlash.Play();
-        if (muzzleFlash) {
+    private void Shoot()
+    {
+        //muzzleFlash.Play();
+        if (muzzleFlash)
+        {
             Debug.Log("MuzzleFlash Not Null");
             // muzzleFlash.SetActive(true);
-            
+
 
             StartCoroutine(StopEffect());
         }
-        
-        
+
+
         //Debug.Log("Shooting");
 
         // Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -79,36 +95,43 @@ public class Shooter : MonoBehaviour {
         // RaycastHit hitInfo;
         GameObject hitMarker = Instantiate(bulletHole, aimState.hitInfo.point, Quaternion.LookRotation(aimState.hitInfo.normal));
         Destroy(hitMarker, 1f);
-        if (aimState.hitInfo.collider.CompareTag("Enemy")) {
+        if (aimState.hitInfo.collider.CompareTag("Enemy"))
+        {
             Debug.Log("Enemy is hit! You win!");
         }
 
         ammo--;
     }
 
-    IEnumerator StopEffect() {
+    IEnumerator StopEffect()
+    {
         yield return new WaitForSeconds(0.3f);
-        if (muzzleFlash) {
+        if (muzzleFlash)
+        {
             // muzzleFlash.SetActive(false);
         }
-        
+
     }
 
-    private void Reload() {
+    private void Reload()
+    {
         StartCoroutine(ShootingCoolDownRoutine());
     }
 
-    IEnumerator ShootingCoolDownRoutine() {
+    IEnumerator ShootingCoolDownRoutine()
+    {
         yield return new WaitForSeconds(1.5f);
         ammo = startAmmo;
         _isReloading = false;
     }
 
-    IEnumerator CoolDownRoutine() {
+    IEnumerator CoolDownRoutine()
+    {
         yield return new WaitForSeconds(5.0f);
     }
 
-    public void _addAmmo() {
+    public void _addAmmo()
+    {
         ammo = startAmmo;
     }
 }
